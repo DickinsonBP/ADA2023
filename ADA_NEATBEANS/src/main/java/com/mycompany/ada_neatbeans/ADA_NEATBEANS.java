761c52f9e3d2ba8e24b3e3631086430d37e16ea9
DickinsonBP <dickinson.bedoya@estudiants.urv.cat>
2023-05-25 15:50:57 +0200

    new neatbeans project

---
+++ ADA_NEATBEANS/src/main/java/com/mycompany/ada_neatbeans/ADA_NEATBEANS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ada_neatbeans;
+
+/**
+ *
+ * @author dicki
+ */
+public class ADA_NEATBEANS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
